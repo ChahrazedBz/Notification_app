9bd8c1b54b4a15dc56e9d02a2a90b91ad00f2220
Chahrazed  Bz <121231703+ChahrazedBz@users.noreply.github.com>
2024-03-16 20:44:33 +0100

    Update notification_app.py

--- notification_app.py
+++ notification_app.py
@@ -5,7 +5,7 @@ from plyer import notification
 if __name__ == "__main__":
     notification.notify(
         title="Simple Notification",
-        message="Everything is gonna be alright Dont worry ",
+        message="Hello ,Have some coffe  ",
         timeout=10,
     )
     time.sleep(7)
